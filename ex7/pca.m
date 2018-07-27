462d67e3c60def702cf4f1f435fc89e15ddeb075
hathackerwang <deanyuton@gmail.com>
2018-07-27 09:54:54 +0800

    ex7

---
+++ ex7/pca.m
@@ -0,0 +1,30 @@
+function [U, S] = pca(X)
+%PCA Run principal component analysis on the dataset X
+%   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
+%   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
+%
+
+% Useful values
+[m, n] = size(X);
+
+% You need to return the following variables correctly.
+U = zeros(n);
+S = zeros(n);
+
+% ====================== YOUR CODE HERE ======================
+
+sigma = zeros(n);
+sigma = 1 / m .* (X' * X);
+
+
+[U,S,V] = svd(sigma);
+
+
+
+
+
+
+
+% =========================================================================
+
+end
