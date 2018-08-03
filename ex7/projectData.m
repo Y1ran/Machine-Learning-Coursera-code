f8ed0d5babdfc5ccf659bd8c76d128e69e3fe29c
hathackerwang <deanyuton@gmail.com>
2018-08-03 20:32:59 +0800

    ex7

---
+++ ex7/projectData.m
@@ -0,0 +1,25 @@
+function Z = projectData(X, U, K)
+%PROJECTDATA Computes the reduced data representation when projecting only 
+%on to the top k eigenvectors
+%   Z = projectData(X, U, K) computes the projection of 
+%   the normalized inputs X into the reduced dimensional space spanned by
+%   the first K columns of U. It returns the projected examples in Z.
+%
+
+% You need to return the following variables correctly.
+Z = zeros(size(X, 1), K);
+
+% ====================== YOUR CODE HERE ======================
+
+
+U_red = U(:,1:K);
+
+Z = X * U_red;
+
+
+
+
+
+% =============================================================
+
+end
---
+++ ex7/recoverData.m
@@ -0,0 +1,21 @@
+function X_rec = recoverData(Z, U, K)
+%RECOVERDATA Recovers an approximation of the original data when using the 
+%projected data
+%   X_rec = RECOVERDATA(Z, U, K) recovers an approximation the 
+%   original data that has been reduced to K dimensions. It returns the
+%   approximate reconstruction in X_rec.
+%
+
+% You need to return the following variables correctly.
+X_rec = zeros(size(Z, 1), size(U, 1));
+
+
+U = U(:,1:K);
+
+X_rec = Z * U';        
+
+
+
+% =============================================================
+
+end
